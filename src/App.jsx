86bd9d3b9c600796af247911d630f2ba488aaa4d
Shakil57375 <shakil5375@gmail.com>
2024-11-20 15:06:31 +0600

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useEffect, useState } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import { Route, Routes, useLocation } from "react-router-dom";
 
-import Loader from './common/Loader';
-import PageTitle from './components/PageTitle';
-import SignIn from './pages/Authentication/SignIn';
-import SignUp from './pages/Authentication/SignUp';
-import DefaultLayout from './layout/DefaultLayout';
-import Dashboard from './pages/Dashboard/Dashboard';
-import Calendar from './pages/Dashboard/Calendar';
-import Profile from './pages/Dashboard/Profile';
-import Tables from './pages/Dashboard/Tables';
-import Settings from './pages/Dashboard/Settings';
+import Loader from "./common/Loader";
+import PageTitle from "./components/PageTitle";
+import DefaultLayout from "./layout/DefaultLayout";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import Calendar from "./pages/Dashboard/Calendar";
+import Profile from "./pages/Dashboard/Profile";
+import Tables from "./pages/Dashboard/Tables";
+import Settings from "./pages/Dashboard/Settings";
+import TermsAndCondition from "./pages/Dashboard/TermsAndCondition";
+import PrivacyAndPolicy from "./pages/Dashboard/PrivacyAndPolicy";
+import LoginForm from "./pages/Authentication/LoginForm";
 
 function Routers() {
   const [loading, setLoading] = useState(true);
@@ -80,7 +81,7 @@ function Routers() {
           element={
             <>
               <PageTitle title="Terms and Conditions" />
-              <Settings />
+              <TermsAndCondition />
             </>
           }
         />
@@ -89,7 +90,7 @@ function Routers() {
           element={
             <>
               <PageTitle title="Privacy And Policy" />
-              <Settings />
+              <PrivacyAndPolicy />
             </>
           }
         />
@@ -98,16 +99,7 @@ function Routers() {
           element={
             <>
               <PageTitle title="Sign In" />
-              <SignIn />
-            </>
-          }
-        />
-        <Route
-          path="/auth/signup"
-          element={
-            <>
-              <PageTitle title="Sign Up" />
-              <SignUp />
+              <LoginForm />
             </>
           }
         />
---
+++ src/components/Authentication/OTPVerificationForm.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const OTPVerificationForm = () => {
+  return (
+    <div>OTPVerificationForm</div>
+  )
+}
+
+export default OTPVerificationForm
