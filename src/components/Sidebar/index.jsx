365859b2607be3c2cd983ea3039ce696168a0a1f
Shakil57375 <shakil57375@gmail.com>
2024-11-23 14:51:16 +0600

    new routes added

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -165,22 +165,81 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                 Make Admin
               </NavLink>
             </li>
-            {/* Subscription */}
-            <li>
-              <NavLink
-                to="/subscription"
-                className={({ isActive }) =>
-                  `group relative flex items-center gap-2.5 rounded-sm py-2 px-8 font-medium text-[#364636] duration-300 ease-in-out ${
-                    isActive
-                      ? "bg-[#8CAB91] !text-white before:content-[''] before:absolute before:top-0 before:left-2 before:h-full before:w-3 before:bg-[#FAF1E6]"
-                      : "hover:bg-[#8CAB91] hover:text-white"
-                  }`
-                }
-              >
-                <ImPower className="text-2xl" />
-                Subscription
-              </NavLink>
-            </li>
+
+            {/* Subscription Dropdown */}
+            <SidebarLinkGroup
+              activeCondition={
+                pathname === "/Subscription" || pathname.includes("auth")
+              }
+            >
+              {(handleClick, open) => (
+                <>
+                  <NavLink
+                    to="#"
+                    className={`group relative flex items-center gap-2.5 rounded-sm py-2 px-8 font-medium text-[#364636] duration-300 ease-in-out hover:bg-[#8CAB91] hover:text-white`}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      sidebarExpanded
+                        ? handleClick()
+                        : setSidebarExpanded(true);
+                    }}
+                  >
+                    <ImPower className="text-2xl" />
+                    Subscription
+                    <svg
+                      className={`absolute right-4 top-1/2 -translate-y-1/2 fill-current ${
+                        open && "rotate-180"
+                      }`}
+                      width="20"
+                      height="20"
+                      viewBox="0 0 20 20"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        clipRule="evenodd"
+                        d="M4.41107 6.9107C4.73651 6.58527 5.26414 6.58527 5.58958 6.9107L10.0003 11.3214L14.4111 6.91071C14.7365 6.58527 15.2641 6.58527 15.5896 6.91071C15.915 7.23614 15.915 7.76378 15.5896 8.08922L10.5896 13.0892C10.2641 13.4147 9.73651 13.4147 9.41107 13.0892L4.41107 8.08922C4.08563 7.76378 4.08563 7.23614 4.41107 6.9107Z"
+                        fill=""
+                      />
+                    </svg>
+                  </NavLink>
+                  {/* Dropdown Menu */}
+                  <div
+                    className={`translate transform overflow-hidden ${
+                      !open && "hidden"
+                    }`}
+                  >
+                    <ul className="mt-4 mb-5.5 flex flex-col gap-2.5 ">
+                      <li>
+                        <NavLink
+                          to="/subscription/subscription"
+                          className={({ isActive }) =>
+                            "group relative flex items-center gap-2.5 hover:!bg-[#8CAB91] hover:!text-white  px-16 py-2 font-medium text-[#364636] duration-300 ease-in-out " +
+                            (isActive &&
+                              "!bg-[#8CAB91] !text-white before:content-[''] before:absolute before:top-0 before:left-2 before:h-full before:w-3 before:bg-[#FAF1E6]")
+                          }
+                        >
+                          Subscription
+                        </NavLink>
+                      </li>
+                      <li>
+                        <NavLink
+                          to="/subscription/couponCode"
+                          className={({ isActive }) =>
+                            "group relative flex items-center hover:!bg-[#8CAB91] hover:!text-white gap-2.5  px-16 py-2  font-medium text-[#364636] duration-300 ease-in-out " +
+                            (isActive &&
+                              "!bg-[#8CAB91] !text-white before:content-[''] before:absolute before:top-0 before:left-2 before:h-full before:w-3 before:bg-[#FAF1E6]")
+                          }
+                        >
+                          Coupon code
+                        </NavLink>
+                      </li>
+                    </ul>
+                  </div>
+                </>
+              )}
+            </SidebarLinkGroup>
             {/* Settings Dropdown */}
             <SidebarLinkGroup
               activeCondition={
@@ -231,7 +290,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           to="/settings/termsAndConditions"
                           className={({ isActive }) =>
                             "group relative flex items-center gap-2.5 hover:!bg-[#8CAB91] hover:!text-white  px-16 py-2 font-medium text-[#364636] duration-300 ease-in-out " +
-                            (isActive && "!bg-[#8CAB91] !text-white before:content-[''] before:absolute before:top-0 before:left-2 before:h-full before:w-3 before:bg-[#FAF1E6]")
+                            (isActive &&
+                              "!bg-[#8CAB91] !text-white before:content-[''] before:absolute before:top-0 before:left-2 before:h-full before:w-3 before:bg-[#FAF1E6]")
                           }
                         >
                           Terms & condition
@@ -242,7 +302,8 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }) => {
                           to="/settings/privacyAndPolicy"
                           className={({ isActive }) =>
                             "group relative flex items-center hover:!bg-[#8CAB91] hover:!text-white gap-2.5  px-16 py-2  font-medium text-[#364636] duration-300 ease-in-out " +
-                            (isActive && "!bg-[#8CAB91] !text-white before:content-[''] before:absolute before:top-0 before:left-2 before:h-full before:w-3 before:bg-[#FAF1E6]")
+                            (isActive &&
+                              "!bg-[#8CAB91] !text-white before:content-[''] before:absolute before:top-0 before:left-2 before:h-full before:w-3 before:bg-[#FAF1E6]")
                           }
                         >
                           Privacy policy
